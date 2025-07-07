88e5447f8bfdbe787c06d942e1711b4ba5b2c038
aidyland911 <34993524+aidyland911@users.noreply.github.com>
2025-07-07 12:47:03 +0300

    Create 02_setup_scandium.sh

---
+++ install/ubuntu/02_setup_scandium.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+HOSTNAME="Scandium"
+IPADDR="10.1.1.21"
+GATEWAY="10.1.1.1"
+IFACE="ens33"
+DNS="10.1.1.254"
+
+echo "Setting hostname to $HOSTNAME"
+hostnamectl set-hostname "$HOSTNAME"
+echo "$HOSTNAME" > /etc/hostname
+
+echo "Backing up existing netplan configs..."
+for f in /etc/netplan/*.yaml; do
+  [ -e "$f" ] && mv "$f" "${f%.yaml}.pak"
+done
+
+tee /etc/netplan/01-static.yaml > /dev/null <<EOF
+network:
+  version: 2
+  ethernets:
+    $IFACE:
+      dhcp4: no
+      addresses: [$IPADDR/24]
+      nameservers:
+        addresses: [$DNS]
+      routes:
+        - to: 0.0.0.0/0
+          via: $GATEWAY
+EOF
+
+chmod 600 /etc/netplan/01-static.yaml
+netplan apply
+ip a show "$IFACE"
